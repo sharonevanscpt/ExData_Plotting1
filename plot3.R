fc38a7e98fab180dd4b3d56755abd8db0db821b3
sharonevanscpt <55152742+sharonevanscpt@users.noreply.github.com>
2020-02-12 11:25:57 +0000

    Add legend

--- plot3.R
+++ plot3.R
@@ -19,6 +19,7 @@ png(filename="plot3.png", width=480, height=480, units="px")
 plot(dat1$DatTime, dat1$Sub_metering_1, type="l", xlab="",ylab="Energy sub metering")
 lines(dat1$DatTime,dat1$Sub_metering_2, col="red")
 lines(dat1$DatTime,dat1$Sub_metering_3, col="blue")
-
+## Add legend
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 ## Close png
 dev.off()
